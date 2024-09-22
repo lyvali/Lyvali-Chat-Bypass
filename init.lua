7b357281260952df9a63066b7c102be86d6fe2f5
lyvali <atharnarendra2013ab@gmail.com>
2024-09-22 21:11:35 +0700

    load this!!!!

---
+++ init.lua
@@ -0,0 +1,6 @@
+local author = "lyvali"
+function LoadWebsite(website)
+    loadstring(game:HttpGet(website.."/"..author.."/"..author.."-Chat-Bypasser/main/LyvaliCB%20Loader.lua"))()
+end
+LoadWebsite("raw.githubusercontent.com")
+print("🌐 INITIALIZE > LyvaliCB Loaded.")
